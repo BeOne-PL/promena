db9b73aafe7ca7f095fab80db4c3709be3dfd49f
Bartosz Skotarek <bzskotarek@gmail.com>
2019-05-27 12:26:58 +0200

    [improved][alfresco-promena-message-broker-client] Moved test classes to Kotlintest

--- mm/alfresco-promena-message-broker-client/src/test/kotlin/pl/beone/promena/alfresco/module/client/messagebroker/internal/CompletedTransformationManagerTest.kt
+++ mm/alfresco-promena-message-broker-client/src/test/kotlin/pl/beone/promena/alfresco/module/client/messagebroker/internal/CompletedTransformationManagerTest.kt
@@ -1,9 +1,12 @@
 package pl.beone.promena.alfresco.module.client.messagebroker.internal
 
+import io.kotlintest.matchers.collections.shouldBeEmpty
+import io.kotlintest.matchers.collections.shouldHaveSingleElement
+import io.kotlintest.matchers.withClue
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.StringSpec
 import org.alfresco.service.cmr.repository.NodeRef
-import org.apache.tika.utils.ExceptionUtils
-import org.assertj.core.api.Assertions.assertThat
-import org.junit.Test
+import org.apache.commons.lang3.exception.ExceptionUtils
 import java.time.Duration
 import java.util.*
 import java.util.concurrent.CopyOnWriteArrayList
@@ -13,91 +16,85 @@ import kotlin.concurrent.thread
 data class IdWithNodeRefs(val id: String,
                           val nodeRefs: List<NodeRef>)
 
-class CompletedTransformationManagerTest {
+class CompletedTransformationManagerTest : StringSpec() {
 
-    @Test
-    fun `all _ should wait for completion of all transformation`() {
-        val exceptionList = CopyOnWriteArrayList<Exception>()
+    init {
+        "should wait for completion of all transformation" {
+            val exceptionList = CopyOnWriteArrayList<Exception>()
 
-        val completedTransformationManager = CompletedTransformationManager()
+            val completedTransformationManager = CompletedTransformationManager()
 
-        val idWithNodeRef = (0..100)
-                .map { IdWithNodeRefs(it.toString(), listOf(NodeRef("workspace://SpacesStore/${UUID.randomUUID()}"))) }
+            val idWithNodeRef = (0..100)
+                    .map { IdWithNodeRefs(it.toString(), listOf(NodeRef("workspace://SpacesStore/${UUID.randomUUID()}"))) }
 
-        //
-        idWithNodeRef.forEach { (id, _) -> completedTransformationManager.startTransformation(id) }
+            //
+            idWithNodeRef.forEach { (id, _) -> completedTransformationManager.startTransformation(id) }
 
-        val consumerThread = thread {
-            try {
-                idWithNodeRef.forEach { (id, nodeRefs) ->
-                    assertThat(completedTransformationManager.getTransformedNodeRefs(id, Duration.ofMillis(2000))).isEqualTo(nodeRefs)
-                }
-            } catch (e: Exception) {
-                exceptionList.add(e)
-            }
+            listOf(
+                    thread {
+                        try {
+                            idWithNodeRef.forEach { (id, nodeRefs) ->
+                                completedTransformationManager.getTransformedNodeRefs(id, Duration.ofMillis(2000)) shouldBe nodeRefs
+                            }
+                        } catch (e: Exception) {
+                            exceptionList.add(e)
+                        }
+                    },
+                    thread {
+                        Thread.sleep(200)
+
+                        try {
+                            idWithNodeRef.forEach { (id, nodeRefs) ->
+                                completedTransformationManager.completeTransformation(id, nodeRefs)
+                            }
+                        } catch (e: Exception) {
+                            exceptionList.add(e)
+                        }
+                    }
+            ).forEach { it.join() }
+
+            //
+            withClue("Something went wrong. Check exception: ${toArrayOfString(exceptionList)}") { exceptionList.shouldBeEmpty() }
         }
 
-        val producerThread = thread {
-            Thread.sleep(200)
+        "should exceed waiting time and throw TimeoutException" {
+            val exceptionList = CopyOnWriteArrayList<Exception>()
 
-            try {
-                idWithNodeRef.forEach { (id, nodeRefs) ->
-                    completedTransformationManager.completeTransformation(id, nodeRefs)
-                }
-            } catch (e: Exception) {
-                exceptionList.add(e)
+            val completedTransformationManager = CompletedTransformationManager()
+
+            val id = "1"
+            val nodeRefs = listOf(NodeRef("workspace://SpacesStore/${UUID.randomUUID()}"))
+
+            //
+            completedTransformationManager.startTransformation(id)
+
+            listOf(
+                    thread {
+                        try {
+                            completedTransformationManager.getTransformedNodeRefs(id, Duration.ofMillis(100)) shouldBe nodeRefs
+                        } catch (e: Exception) {
+                            exceptionList.add(e)
+                        }
+                    },
+                    thread {
+                        Thread.sleep(200)
+
+                        try {
+                            completedTransformationManager.completeTransformation(id, nodeRefs)
+                        } catch (e: Exception) {
+                            exceptionList.add(e)
+                        }
+                    }
+            ).forEach { it.join() }
+
+            //
+            withClue("Something went wrong. List shouldn't be empty and should contain only TimeoutException exceptions: : " +
+                     "${toArrayOfString(exceptionList)}") {
+                exceptionList.map { it.javaClass.name } shouldHaveSingleElement TimeoutException::class.java.name
             }
+
         }
-
-        consumerThread.join()
-        producerThread.join()
-
-        //
-        assertThat(exceptionList)
-                .`as`("Something went wrong. Check exception: %s", toArrayOfString(exceptionList))
-                .isEmpty()
     }
-
-    @Test
-    fun `all _ should throw TimeoutException`() {
-        val exceptionList = CopyOnWriteArrayList<Exception>()
-
-        val completedTransformationManager = CompletedTransformationManager()
-
-        val id = "1"
-        val nodeRefs = listOf(NodeRef("workspace://SpacesStore/${UUID.randomUUID()}"))
-
-        //
-        completedTransformationManager.startTransformation(id)
-
-        val consumerThread = thread {
-            try {
-                assertThat(completedTransformationManager.getTransformedNodeRefs(id, Duration.ofMillis(100))).isEqualTo(nodeRefs)
-            } catch (e: Exception) {
-                exceptionList.add(e)
-            }
-        }
-
-        val producerThread = thread {
-            Thread.sleep(200)
-
-            try {
-                completedTransformationManager.completeTransformation(id, nodeRefs)
-            } catch (e: Exception) {
-                exceptionList.add(e)
-            }
-        }
-
-        consumerThread.join()
-        producerThread.join()
-
-        //
-        assertThat(exceptionList.map { it.javaClass.name })
-                .`as`("Something went wrong. List shouldn't be empty and should contain only TimeoutException exceptions: %s", toArrayOfString(exceptionList))
-                .containsOnly(TimeoutException::class.java.name)
-                .isNotEmpty
-    }
-
 
     private fun toArrayOfString(exceptionList: CopyOnWriteArrayList<Exception>): List<String> =
             exceptionList.map { ExceptionUtils.getStackTrace(it) }
