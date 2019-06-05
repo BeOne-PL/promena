aa5a064230c040243b5b2b6cbbff1fd3beb869a8
Bartosz Skotarek <bzskotarek@gmail.com>
2019-06-05 11:18:50 +0200

    [fixed][promena-connector-activemq] Determining consumer selector

--- extension/connector/activemq/promena-connector-activemq-configuration/src/main/kotlin/pl/beone/promena/connector/activemq/configuration/delivery/jms/TransformerConsumerContext.kt
+++ extension/connector/activemq/promena-connector-activemq-configuration/src/main/kotlin/pl/beone/promena/connector/activemq/configuration/delivery/jms/TransformerConsumerContext.kt
@@ -1,37 +1,49 @@
 package pl.beone.promena.connector.activemq.configuration.delivery.jms
 
 import org.apache.activemq.command.ActiveMQQueue
+import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.env.MapPropertySource
 import org.springframework.core.env.StandardEnvironment
 import org.springframework.jms.core.JmsTemplate
+import pl.beone.promena.connector.activemq.delivery.jms.PromenaJmsHeader.PROMENA_TRANSFORMER_ID
 import pl.beone.promena.connector.activemq.delivery.jms.TransformerConsumer
 import pl.beone.promena.core.contract.transformation.TransformationUseCase
 import pl.beone.promena.core.contract.transformer.config.TransformerConfig
+import pl.beone.promena.transformer.contract.Transformer
 
 @Configuration
 class TransformerConsumerContext {
 
-    @Bean
-    fun transformerConsumer(environment: StandardEnvironment,
-                            transformerConfigs: List<TransformerConfig>,
-                            jmsTemplate: JmsTemplate,
-                            transformationUseCase: TransformationUseCase): TransformerConsumer {
-        addSelectorToEnvironment(environment, transformerConfigs)
-
-        return TransformerConsumer(jmsTemplate,
-                ActiveMQQueue(environment.getRequiredProperty("promena.connector.activemq.consumer.queue.response")),
-                ActiveMQQueue(environment.getRequiredProperty("promena.connector.activemq.consumer.queue.response.error")),
-                transformationUseCase)
+    companion object {
+        private val logger = LoggerFactory.getLogger(TransformerConsumerContext::class.java)
     }
 
-    private fun addSelectorToEnvironment(environment: StandardEnvironment, transformerConfigs: List<TransformerConfig>) {
-        val selector = transformerConfigs.joinToString(" OR ") { "transformerId = '$it'" }
+    @Bean
+    fun transformerConsumer(environment: StandardEnvironment,
+                            transformers: List<Transformer>,
+                            transformerConfig: TransformerConfig,
+                            jmsTemplate: JmsTemplate,
+                            transformationUseCase: TransformationUseCase): TransformerConsumer {
+        addSelectorToEnvironment(environment, transformerConfig, transformers)
+
+        return TransformerConsumer(jmsTemplate,
+                                   ActiveMQQueue(environment.getRequiredProperty("promena.connector.activemq.consumer.queue.response")),
+                                   ActiveMQQueue(environment.getRequiredProperty("promena.connector.activemq.consumer.queue.response.error")),
+                                   transformationUseCase)
+    }
+
+    private fun addSelectorToEnvironment(environment: StandardEnvironment,
+                                         transformerConfig: TransformerConfig,
+                                         transformers: List<Transformer>) {
+        val selector =
+                transformers.joinToString(" OR ") { "$PROMENA_TRANSFORMER_ID = '${transformerConfig.getTransformationId(it)}'" }
+
+        logger.info("Set transformer consumer selector to: {}", selector)
 
         environment.propertySources.addLast(
-                MapPropertySource("transformerConsumer",
-                        mapOf("promena.connector.activemq.consumer.queue.request.selector" to selector))
+                MapPropertySource("transformerConsumer", mapOf("promena.connector.activemq.consumer.queue.request.selector" to selector))
         )
     }
 }
