0cfc4f4aa0ff0960cd8d4ea41687878d81c53e0d
Bartosz Skotarek <bzskotarek@gmail.com>
2019-06-07 12:55:12 +0200

    [added][promena-connector-activemq-delivery-jms] Tests

--- extension/connector/activemq/delivery/promena-connector-activemq-delivery-jms/src/main/kotlin/pl/beone/promena/connector/activemq/delivery/jms/HeadersToSentBackDeterminer.kt
+++ extension/connector/activemq/delivery/promena-connector-activemq-delivery-jms/src/main/kotlin/pl/beone/promena/connector/activemq/delivery/jms/HeadersToSentBackDeterminer.kt
@@ -2,6 +2,11 @@ package pl.beone.promena.connector.activemq.delivery.jms
 
 internal class HeadersToSentBackDeterminer {
 
-    fun determine(headers: Map<String, Any>): Map<String, Any> =
-            headers.filter { (key, _) -> key.startsWith("send_back_") }.toMap()
+    companion object {
+        private const val PROMENA_SEND_BACK_PREFIX = "send_back_"
+    }
+
+    fun determine(headers: Map<String, Any>): Map<String, Any> {
+        return headers.filter { (key, _) -> key.startsWith(PROMENA_SEND_BACK_PREFIX) }.toMap()
+    }
 }
