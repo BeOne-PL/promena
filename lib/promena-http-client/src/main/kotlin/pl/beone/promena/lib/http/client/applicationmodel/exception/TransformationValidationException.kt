5e8958545883107654caa7330e38ffdfbf75d08d
Bartosz Skotarek <bzskotarek@gmail.com>
2019-06-19 08:33:51 +0200

    [removed][promena-http-client] No longed used

--- lib/promena-http-client/src/main/kotlin/pl/beone/promena/lib/http/client/applicationmodel/exception/TransformationValidationException.kt
+++
@@ -1,3 +0,0 @@
-package pl.beone.promena.lib.http.client.applicationmodel.exception
-
-class TransformationValidationException(message: String, exception: Exception? = null) : RuntimeException(message, exception)
--- lib/promena-http-client/src/main/kotlin/pl/beone/promena/lib/http/client/contract/request/TransformationExecutor.kt
+++
@@ -1,8 +0,0 @@
-package pl.beone.promena.lib.http.client.contract.request
-
-import pl.beone.promena.core.contract.communication.CommunicationParameters
-
-interface TransformationExecutor {
-
-    fun execute(transformerId: String, bytes: ByteArray, parameters: CommunicationParameters, timeout: Long): ByteArray
-}
