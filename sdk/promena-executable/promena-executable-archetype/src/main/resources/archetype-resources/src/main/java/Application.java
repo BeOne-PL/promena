8e86a6949d4ec7d8e7221fe62d4629552a34b8b2
Bartosz Skotarek <bzskotarek@gmail.com>
2019-07-08 15:33:25 +0200

    [added][promena-executable-archetype] Archetype to generate Promena docker images

---
+++ sdk/promena-executable/promena-executable-archetype/src/main/resources/archetype-resources/src/main/java/Application.java
@@ -0,0 +1,13 @@
+package ${package};
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"pl.beone.promena"})
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
