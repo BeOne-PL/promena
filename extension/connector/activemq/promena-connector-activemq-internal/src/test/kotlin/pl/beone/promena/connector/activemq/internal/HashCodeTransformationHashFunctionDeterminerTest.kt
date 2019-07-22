14e1151fd12a5dc9255d317e1a373df0fa378016
Bartosz Skotarek <bzskotarek@gmail.com>
2019-07-22 12:52:31 +0200

    [added][promena-connector-activemq] Test to HashCodeTransformationHashFunctionDeterminer

--- extension/connector/activemq/promena-connector-activemq-internal/src/test/kotlin/pl/beone/promena/connector/activemq/internal/HashCodeTransformationHashFunctionDeterminerTest.kt
+++ extension/connector/activemq/promena-connector-activemq-internal/src/test/kotlin/pl/beone/promena/connector/activemq/internal/HashCodeTransformationHashFunctionDeterminerTest.kt
@@ -1,13 +1,25 @@
 package pl.beone.promena.connector.activemq.internal
 
+import io.kotlintest.shouldBe
 import org.junit.Test
-
-import org.junit.Assert.*
 
 class HashCodeTransformationHashFunctionDeterminerTest {
 
-    // TODO test it
+    companion object {
+        private val transformationHashFunctionDeterminer = HashCodeTransformationHashFunctionDeterminer()
+    }
+
     @Test
     fun determine() {
+        val hashCode = "-2100906783"
+
+        transformationHashFunctionDeterminer.determine(listOf("converter", "barcode")) shouldBe hashCode
+        transformationHashFunctionDeterminer.determine(listOf("barcode", "converter")) shouldBe hashCode
+    }
+
+    @Test
+    fun `determine _ different order`() {
+        transformationHashFunctionDeterminer.determine(listOf("converter", "barcode", "report")) shouldBe
+                transformationHashFunctionDeterminer.determine(listOf("report", "converter", "barcode"))
     }
 }
