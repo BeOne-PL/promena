b6ca415403a4f6e39d05deb5e0c624466018c08f
Bartosz Skotarek <bzskotarek@gmail.com>
2019-07-30 12:54:02 +0200

    [improved][promena-docker-maven-plugin] Refactor, logging messages [added][promena-docker-maven-plugin] Logging building Docker image

--- lib/promena-docker-maven-plugin/src/main/kotlin/pl/beone/promena/maven/plugin/docker/BuildMojo.kt
+++ lib/promena-docker-maven-plugin/src/main/kotlin/pl/beone/promena/maven/plugin/docker/BuildMojo.kt
@@ -3,22 +3,17 @@ package pl.beone.promena.maven.plugin.docker
 import com.github.dockerjava.api.DockerClient
 import com.github.dockerjava.core.DefaultDockerClientConfig
 import com.github.dockerjava.core.DockerClientBuilder
-import com.github.dockerjava.core.command.BuildImageResultCallback
 import org.apache.maven.artifact.Artifact
 import org.apache.maven.plugin.AbstractMojo
 import org.apache.maven.plugin.descriptor.PluginDescriptor
 import org.apache.maven.plugins.annotations.Mojo
 import org.apache.maven.plugins.annotations.Parameter
-import org.slf4j.LoggerFactory
 import java.io.File
+import java.net.URL
 import java.net.URLClassLoader
 
 @Mojo(name = "build")
 class BuildMojo : AbstractMojo() {
-
-    companion object {
-        private val logger = LoggerFactory.getLogger(BuildMojo::class.java)
-    }
 
     @Parameter(defaultValue = "\${plugin}")
     private lateinit var pluginDescriptor: PluginDescriptor
@@ -49,14 +44,22 @@ class BuildMojo : AbstractMojo() {
 
         val transformerDockerfileFragments = getTransformerDockerfileFragments()
 
-        log.info("Building docker image <${getImageFullName()}> using <$dockerfile, $dockerfileFragment, $appJar> from <$context>")
+        log.info("Building docker image: ${getImageFullName()}...")
+        if (log.isDebugEnabled) {
+            log.debug(" Jar: ${File(outputDirectory, appJar).path}")
+            log.debug(" Dockerfile: ${File(context, dockerfile).path}")
+            log.debug(" Replacing <\${DOCKERFILE-FRAGMENT}> in Dockerfile using <$dockerfileFragment> files from transformers")
+        }
 
-        onProcessedDockerfile(transformerDockerfileFragments) {
-            dockerClient.buildImageCmd(it)
+        onProcessedDockerfile(transformerDockerfileFragments) { dockerfile ->
+            dockerClient.buildImageCmd(dockerfile)
                 .withTags(setOf(getImageFullName()))
-                .exec(BuildImageResultCallback())
+                .exec(LoggerBuildImageResultCallback(log))
                 .awaitImageId()
         }
+
+        log.info("\n")
+        log.info("Finished building docker image: ${getImageFullName()}")
     }
 
     private fun getImageFullName(): String =
@@ -70,20 +73,20 @@ class BuildMojo : AbstractMojo() {
             .build()
 
     private fun getTransformerDockerfileFragments(): List<String> =
-        pluginDescriptor.artifacts.mapNotNull {
-            val dockerfileFragment = URLClassLoader(arrayOf(it.file.toURI().toURL()))
-                .getResource(dockerfileFragment)?.readText()
-
-            if (dockerfileFragment != null) {
-                logger.info("Found Dockerfile fragment in <${it.getDescription()}>")
-                "# ${it.getDescription()}\n$dockerfileFragment"
-            } else {
-                null
+        pluginDescriptor.artifacts.map { it.getDescription() to it.getResourceUrl(dockerfileFragment) }
+            .filter { (_, resourceUrl) -> resourceUrl != null }
+            .also {
+                log.info("Found $dockerfileFragment in <${it.size}> transformers:")
+                it.forEach { (artifactDescription, _) -> log.info("> $artifactDescription") }
             }
-        }
+            .map { (artifactDescription, resourceUrl) -> "# $artifactDescription" + "\n" + resourceUrl!!.readText() }
 
     private fun Artifact.getDescription(): String =
         this.groupId + ":" + this.artifactId + ":" + this.version
+
+    private fun Artifact.getResourceUrl(resourcePath: String): URL? =
+        URLClassLoader(arrayOf(file.toURI().toURL()))
+            .getResource(resourcePath)
 
     private fun onProcessedDockerfile(transformerDockerfileFragments: List<String>, toRun: (dockerfile: File) -> Unit) {
         val transformerDockerfile = transformerDockerfileFragments.joinToString("\n\n")
@@ -101,25 +104,27 @@ class BuildMojo : AbstractMojo() {
     private fun copyJar(): File {
         val appJarFile = File(outputDirectory, appJar)
         val processedAppJarFile = File(context, appJar)
-        logger.debug("Copying jar from <$appJarFile> to <$processedAppJarFile>")
 
+        log.debug("Copying jar from <$appJarFile> to <$processedAppJarFile>...")
         appJarFile.copyTo(processedAppJarFile, true)
+        log.debug("Finished copying jar from <$appJarFile> to <$processedAppJarFile>")
+
         return processedAppJarFile
     }
 
     private fun processDockerfile(transformerDockerfile: String): File {
         val dockerfileFile = File(context, dockerfile)
         val processedDockerfileFile = File(context, "Dockerfile-processed")
-        logger.debug("Writing to processed Dockerfile <$processedDockerfileFile> based on Dockerfile <$dockerfileFile>")
 
         val processedDockerfileContent =
             dockerfileFile
                 .readText()
                 .replace("\${DOCKERFILE-FRAGMENT}", transformerDockerfile)
                 .replace("\${APP_JAR}", appJar)
-        logger.debug("Processed Dockerfile:\n$processedDockerfileContent")
+        log.debug("Generated Dockerfile:" + "\n" + processedDockerfileContent)
 
-        processedDockerfileFile.writeText(processedDockerfileContent)
-        return processedDockerfileFile
+        return processedDockerfileFile.apply {
+            writeText(processedDockerfileContent)
+        }
     }
 }
---
+++ lib/promena-docker-maven-plugin/src/main/kotlin/pl/beone/promena/maven/plugin/docker/LoggerBuildImageResultCallback.kt
@@ -0,0 +1,13 @@
+package pl.beone.promena.maven.plugin.docker
+
+import com.github.dockerjava.api.model.BuildResponseItem
+import com.github.dockerjava.core.command.BuildImageResultCallback
+import org.apache.maven.plugin.logging.Log
+
+class LoggerBuildImageResultCallback(private val log: Log) : BuildImageResultCallback() {
+
+    override fun onNext(item: BuildResponseItem) {
+        super.onNext(item)
+        log.info(item.stream?.trimEnd('\n'))
+    }
+}
