14dbbdea1c9eccb11c9890df5f4cb7c2ab01c862
Bartosz Skotarek <bzskotarek@gmail.com>
2019-07-31 14:28:13 +0200

    [improved][promena-connector-http] Logging at startup

---
+++ extension/connector/http/promena-connector-http-configuration/src/main/kotlin/pl/beone/promena/connector/http/configuration/HttpConnectorModuleLogger.kt
@@ -0,0 +1,23 @@
+package pl.beone.promena.connector.http.configuration
+
+import org.slf4j.LoggerFactory
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.DependsOn
+import org.springframework.core.env.Environment
+import javax.annotation.PostConstruct
+
+@Configuration
+@DependsOn("transformationConsumer")
+class HttpConnectorModuleLogger(
+    private val environment: Environment
+) {
+
+    companion object {
+        private val logger = LoggerFactory.getLogger(HttpConnectorModuleLogger::class.java)
+    }
+
+    @PostConstruct
+    private fun log() {
+        logger.info("Registered <http> connector: <port: {}>", environment.getRequiredProperty("server.port"))
+    }
+}
