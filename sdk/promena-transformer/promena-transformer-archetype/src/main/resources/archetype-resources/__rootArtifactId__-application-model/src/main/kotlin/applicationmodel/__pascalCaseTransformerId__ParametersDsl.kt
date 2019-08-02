bf3db3e6d79f525376c276a0e3394d394492d51a
Bartosz Skotarek <bzskotarek@gmail.com>
2019-08-02 10:44:12 +0200

    [added][promena-transformer-archetype] Adapted to new transformer API functions

--- sdk/promena-transformer/promena-transformer-archetype/src/main/resources/archetype-resources/__rootArtifactId__-application-model/src/main/kotlin/applicationmodel/__pascalCaseTransformerId__ParametersDsl.kt
+++ sdk/promena-transformer/promena-transformer-archetype/src/main/resources/archetype-resources/__rootArtifactId__-application-model/src/main/kotlin/applicationmodel/__pascalCaseTransformerId__ParametersDsl.kt
@@ -7,6 +7,7 @@ import pl.beone.promena.transformer.contract.model.Parameters
 import pl.beone.promena.transformer.contract.transformation.Transformation
 import pl.beone.promena.transformer.contract.transformation.singleTransformation
 import pl.beone.promena.transformer.internal.model.parameters.MapParameters
+import pl.beone.promena.transformer.internal.model.parameters.addIfNotNull
 import pl.beone.promena.transformer.internal.model.parameters.emptyParameters
 import pl.beone.promena.transformer.internal.model.parameters.plus
 
@@ -15,5 +16,5 @@ fun ${camelCaseTransformerId}Transformation(targetMediaType: MediaType, paramete
 
 fun ${camelCaseTransformerId}Parameters(example: String, example2: String? = null): MapParameters =
     emptyParameters() +
-            (${pascalCaseTransformerId}Constants.Parameters.EXAMPLE to example) +
+            (${pascalCaseTransformerId}Constants.Parameters.EXAMPLE to example) addIfNotNull
             (${pascalCaseTransformerId}Constants.Parameters.EXAMPLE2 to example2)
