72e4464bccd45ecef7f0755e0f1b7a0affdda5e5
Bartosz Skotarek <bzskotarek@gmail.com>
2019-08-02 13:07:28 +0200

    [added][promena-extension-archetype] Extension archetype

---
+++ sdk/promena-extension/promena-extension-archetype/src/main/resources/archetype-resources/__rootArtifactId__-configuration/src/main/kotlin/pl/beone/promena/configuration/__pascalCaseArtifactId__ModuleContext.kt
@@ -0,0 +1,10 @@
+package pl.beone.promena.configuration
+
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.PropertySource
+
+@Configuration
+@ComponentScan(basePackages = ["${package}.configuration"])
+@PropertySource("classpath:module-${rootArtifactId}.properties")
+class ${pascalCaseArtifactId}ModuleContext
