1824999966d8c8174fb982337fd3c80f5ec4bc88
Bartosz Skotarek <bzskotarek@gmail.com>
2019-08-03 12:03:34 +0200

    [improved][promena-connector-activemq] Quality of logger messages at the startup

--- extension/connector/activemq/promena-connector-activemq-configuration/src/main/kotlin/pl/beone/promena/connector/activemq/configuration/ActiveMQConnectorLogger.kt
+++ extension/connector/activemq/promena-connector-activemq-configuration/src/main/kotlin/pl/beone/promena/connector/activemq/configuration/ActiveMQConnectorLogger.kt
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.jms.JmsProperties
 import org.springframework.boot.autoconfigure.jms.activemq.ActiveMQProperties
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.DependsOn
+import org.springframework.core.env.Environment
 import javax.annotation.PostConstruct
 
 @Configuration
 @DependsOn("transformationConsumer")
 class ActiveMQConnectorLogger(
+    private val environment: Environment,
     private val activeMQProperties: ActiveMQProperties,
     private val jmsProperties: JmsProperties
 ) {
@@ -22,6 +24,9 @@ class ActiveMQConnectorLogger(
     private fun log() {
         logger.info {
             "Registered <activemq> connector: <broker-url: ${activeMQProperties.brokerUrl}>, " +
+                    "<queue.request: ${environment.getRequiredProperty("promena.connector.activemq.consumer.queue.request")}>, " +
+                    "<queue.response: ${environment.getRequiredProperty("promena.connector.activemq.consumer.queue.response")}>, " +
+                    "<queue.response.error: ${environment.getRequiredProperty("promena.connector.activemq.consumer.queue.response.error")}>, " +
                     "<pool.max-connections: ${activeMQProperties.pool.maxConnections}>, " +
                     "<listener.concurrency: ${jmsProperties.listener.concurrency}>, " +
                     "<listener.max-concurrency: ${jmsProperties.listener.maxConcurrency}>"
