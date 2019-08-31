7339627bdd2a38fa453b18dc635d59acfcbf99a5
Bartosz Skotarek <bzskotarek@gmail.com>
2019-08-31 09:10:30 +0200

    [improved][promena-extension-archetype] Adapted dependencies to new POM parents [added][promena-extension-archetype] Example configuration class to configuration package

---
+++ sdk/promena-extension/promena-extension-archetype/src/main/resources/archetype-resources/__rootArtifactId__-configuration/src/main/kotlin/configuration/ExampleContext.kt
@@ -0,0 +1,12 @@
+package ${package}.configuration
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class ExampleContext {
+
+//    @Bean
+//    fun example() =
+//        Example()
+}
