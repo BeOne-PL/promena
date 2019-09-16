7a13f0dbb8e7390fb2f708fe763438573e00025a
Bartosz Skotarek <bzskotarek@gmail.com>
2019-09-16 19:16:25 +0200

    [improved][promena-transformer-archetype] DslTest - static import

--- sdk/promena-transformer/promena-transformer-archetype/src/main/resources/archetype-resources/__rootArtifactId__-application-model/src/test/kotlin/applicationmodel/__pascalCaseTransformerId__DslTest.kt
+++ sdk/promena-transformer/promena-transformer-archetype/src/main/resources/archetype-resources/__rootArtifactId__-application-model/src/test/kotlin/applicationmodel/__pascalCaseTransformerId__DslTest.kt
@@ -3,15 +3,18 @@ package ${package}.applicationmodel
 import io.kotlintest.shouldBe
 import io.kotlintest.shouldThrow
 import org.junit.jupiter.api.Test
-import pl.beone.promena.transformer.applicationmodel.mediatype.MediaTypeConstants
+import pl.beone.promena.transformer.applicationmodel.mediatype.MediaTypeConstants.TEXT_PLAIN
+import ${package}.applicationmodel.${pascalCaseTransformerId}Constants.TRANSFORMER_ID
+import ${package}.applicationmodel.${pascalCaseTransformerId}ParametersConstants.EXAMPLE
+import ${package}.applicationmodel.${pascalCaseTransformerId}ParametersConstants.EXAMPLE2
 
 class ${pascalCaseTransformerId}DslTest {
 
     @Test
     fun ${camelCaseTransformerId}Parameters() {
         ${camelCaseTransformerId}Parameters(example = "test", example2 = "test2").let {
-            it.get(${pascalCaseTransformerId}ParametersConstants.EXAMPLE) shouldBe "test"
-            it.get(${pascalCaseTransformerId}ParametersConstants.EXAMPLE2) shouldBe "test2"
+            it.get(EXAMPLE, String::class.java) shouldBe "test"
+            it.get(EXAMPLE2, String::class.java) shouldBe "test2"
         }
     }
 
@@ -19,15 +22,16 @@ class ${pascalCaseTransformerId}DslTest {
     fun `${camelCaseTransformerId}Parameters _ no optional example2 parameter _ should throw NoSuchElementException`() {
         shouldThrow<NoSuchElementException> {
             ${camelCaseTransformerId}Parameters(example = "test")
-                .get(${pascalCaseTransformerId}ParametersConstants.EXAMPLE2)
+                .get(EXAMPLE2)
         }
     }
 
     @Test
     fun ${camelCaseTransformerId}Transformation() {
-        ${camelCaseTransformerId}Transformation(MediaTypeConstants.TEXT_PLAIN, ${camelCaseTransformerId}Parameters(example = "test")).let {
-            it.transformerId shouldBe ${pascalCaseTransformerId}Constants.TRANSFORMER_ID
-            it.targetMediaType shouldBe MediaTypeConstants.TEXT_PLAIN
+        ${camelCaseTransformerId}Transformation(TEXT_PLAIN, ${camelCaseTransformerId}Parameters(example = "test")).let {
+            it.transformerId shouldBe TRANSFORMER_ID
+            it.targetMediaType shouldBe TEXT_PLAIN
+            it.parameters.get(EXAMPLE, String::class.java) shouldBe "test"
         }
     }
 }
