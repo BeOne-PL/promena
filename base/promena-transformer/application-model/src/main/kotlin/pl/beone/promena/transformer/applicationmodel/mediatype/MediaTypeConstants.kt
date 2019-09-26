f1b70efc2cb982c3e4b6401037f20e3a96d360d1
Bartosz Skotarek <bzskotarek@gmail.com>
2019-09-26 11:33:45 +0200

    [improved][promena-core] Replaced creating media types using factory methods with DSL

--- base/promena-transformer/application-model/src/main/kotlin/pl/beone/promena/transformer/applicationmodel/mediatype/MediaTypeConstants.kt
+++ base/promena-transformer/application-model/src/main/kotlin/pl/beone/promena/transformer/applicationmodel/mediatype/MediaTypeConstants.kt
@@ -3,163 +3,163 @@ package pl.beone.promena.transformer.applicationmodel.mediatype
 object MediaTypeConstants {
 
     @JvmField
-    val APPLICATION_OCTET_STREAM = MediaType.of("application/octet-stream", Charsets.UTF_8)
+    val APPLICATION_OCTET_STREAM = mediaType("application/octet-stream")
     @JvmField
-    val APPLICATION_ECMASCRIPT = MediaType.of("application/ecmascript", Charsets.UTF_8)
+    val APPLICATION_ECMASCRIPT = mediaType("application/ecmascript")
     @JvmField
-    val APPLICATION_EPUB_ZIP = MediaType.of("application/epub+zip", Charsets.UTF_8)
+    val APPLICATION_EPUB_ZIP = mediaType("application/epub+zip")
     @JvmField
-    val APPLICATION_JAVASCRIPT = MediaType.of("application/javascript", Charsets.UTF_8)
+    val APPLICATION_JAVASCRIPT = mediaType("application/javascript")
     @JvmField
-    val APPLICATION_JAVA_ARCHIVE = MediaType.of("application/java-archive", Charsets.UTF_8)
+    val APPLICATION_JAVA_ARCHIVE = mediaType("application/java-archive")
     @JvmField
-    val APPLICATION_JSON = MediaType.of("application/json", Charsets.UTF_8)
+    val APPLICATION_JSON = mediaType("application/json")
     @JvmField
-    val APPLICATION_MSWORD = MediaType.of("application/msword", Charsets.UTF_8)
+    val APPLICATION_MSWORD = mediaType("application/msword")
     @JvmField
-    val APPLICATION_VND_MS_WORD_DOCUMENT_MACROENABLED_12 = MediaType.of("application/vnd.ms-word.document.macroEnabled.12", Charsets.UTF_8)
+    val APPLICATION_VND_MS_WORD_DOCUMENT_MACROENABLED_12 = mediaType("application/vnd.ms-word.document.macroEnabled.12")
     @JvmField
-    val APPLICATION_OGG = MediaType.of("application/ogg", Charsets.UTF_8)
+    val APPLICATION_OGG = mediaType("application/ogg")
     @JvmField
-    val APPLICATION_PDF = MediaType.of("application/pdf", Charsets.UTF_8)
+    val APPLICATION_PDF = mediaType("application/pdf")
     @JvmField
-    val APPLICATION_RTF = MediaType.of("application/rtf", Charsets.UTF_8)
+    val APPLICATION_RTF = mediaType("application/rtf")
     @JvmField
-    val APPLICATION_TYPESCRIPT = MediaType.of("application/typescript", Charsets.UTF_8)
+    val APPLICATION_TYPESCRIPT = mediaType("application/typescript")
     @JvmField
-    val APPLICATION_VND_AMAZON_EBOOK = MediaType.of("application/vnd.amazon.ebook", Charsets.UTF_8)
+    val APPLICATION_VND_AMAZON_EBOOK = mediaType("application/vnd.amazon.ebook")
     @JvmField
-    val APPLICATION_VND_APPLE_INSTALLER_XML = MediaType.of("application/vnd.apple.installer+xml", Charsets.UTF_8)
+    val APPLICATION_VND_APPLE_INSTALLER_XML = mediaType("application/vnd.apple.installer+xml")
     @JvmField
-    val APPLICATION_VND_MOZILLA_XUL_XML = MediaType.of("application/vnd.mozilla.xul+xml", Charsets.UTF_8)
+    val APPLICATION_VND_MOZILLA_XUL_XML = mediaType("application/vnd.mozilla.xul+xml")
     @JvmField
-    val APPLICATION_VND_MS_EXCEL = MediaType.of("application/vnd.ms-excel", Charsets.UTF_8)
+    val APPLICATION_VND_MS_EXCEL = mediaType("application/vnd.ms-excel")
     @JvmField
-    val APPLICATION_VND_MS_EXCEL_SHEET_MACRO_ENABLED_12 = MediaType.of("application/application/vnd.ms-excel.sheet.macroEnabled.12", Charsets.UTF_8)
+    val APPLICATION_VND_MS_EXCEL_SHEET_MACROENABLED_12 = mediaType("application/application/vnd.ms-excel.sheet.macroEnabled.12")
     @JvmField
-    val APPLICATION_VND_MS_EXCEL_TEMPLATE_MACRO_ENABLED_12 = MediaType.of("application/vnd.ms-excel.template.macroEnabled.12", Charsets.UTF_8)
+    val APPLICATION_VND_MS_EXCEL_TEMPLATE_MACROENABLED_12 = mediaType("application/vnd.ms-excel.template.macroEnabled.12")
     @JvmField
-    val APPLICATION_VND_MS_FONTOBJECT = MediaType.of("application/vnd.ms-fontobject", Charsets.UTF_8)
+    val APPLICATION_VND_MS_FONTOBJECT = mediaType("application/vnd.ms-fontobject")
     @JvmField
-    val APPLICATION_VND_MS_POWERPOINT = MediaType.of("application/vnd.ms-powerpoint", Charsets.UTF_8)
+    val APPLICATION_VND_MS_POWERPOINT = mediaType("application/vnd.ms-powerpoint")
     @JvmField
-    val APPLICATION_VND_MS_POWERPOINT_TEMPLATE_MACROENABLED_12 = MediaType.of("application/vnd.ms-powerpoint.template.macroEnabled.12", Charsets.UTF_8)
+    val APPLICATION_VND_MS_POWERPOINT_TEMPLATE_MACROENABLED_12 = mediaType("application/vnd.ms-powerpoint.template.macroEnabled.12")
     @JvmField
-    val APPLICATION_VND_MS_POWERPOINT_SLIDESHOW_MACROENABLED_12 = MediaType.of("application/vnd.ms-powerpoint.slideshow.macroEnabled.12", Charsets.UTF_8)
+    val APPLICATION_VND_MS_POWERPOINT_SLIDESHOW_MACROENABLED_12 = mediaType("application/vnd.ms-powerpoint.slideshow.macroEnabled.12")
     @JvmField
     val APPLICATION_VND_OPENXMLFORMATS_OFFICEDOCUMENT_PRESENTATIONML_SLIDESHOW =
-        MediaType.of("application/vnd.openxmlformats-officedocument.presentationml.slideshow", Charsets.UTF_8)
+        mediaType("application/vnd.openxmlformats-officedocument.presentationml.slideshow")
     @JvmField
     val APPLICATION_VND_MS_POWERPOINT_PRESENTATION_MACROENABLED_12 =
-        MediaType.of("application/vnd.ms-powerpoint.presentation.macroEnabled.12", Charsets.UTF_8)
+        mediaType("application/vnd.ms-powerpoint.presentation.macroEnabled.12")
     @JvmField
-    val APPLICATION_VND_OASIS_OPENDOCUMENT_PRESENTATION = MediaType.of("application/vnd.oasis.opendocument.presentation", Charsets.UTF_8)
+    val APPLICATION_VND_OASIS_OPENDOCUMENT_PRESENTATION = mediaType("application/vnd.oasis.opendocument.presentation")
     @JvmField
-    val APPLICATION_VND_OASIS_OPENDOCUMENT_PRESENTATION_TEMPLATE = MediaType.of("application/vnd.oasis.opendocument.presentation-template", Charsets.UTF_8)
+    val APPLICATION_VND_OASIS_OPENDOCUMENT_PRESENTATION_TEMPLATE = mediaType("application/vnd.oasis.opendocument.presentation-template")
     @JvmField
-    val APPLICATION_VND_OASIS_OPENDOCUMENT_SPREADSHEET = MediaType.of("application/vnd.oasis.opendocument.spreadsheet", Charsets.UTF_8)
+    val APPLICATION_VND_OASIS_OPENDOCUMENT_SPREADSHEET = mediaType("application/vnd.oasis.opendocument.spreadsheet")
     @JvmField
-    val APPLICATION_VND_OASIS_OPENDOCUMENT_SPREADSHEET_TEMPLATE = MediaType.of("application/vnd.oasis.opendocument.spreadsheet-template", Charsets.UTF_8)
+    val APPLICATION_VND_OASIS_OPENDOCUMENT_SPREADSHEET_TEMPLATE = mediaType("application/vnd.oasis.opendocument.spreadsheet-template")
     @JvmField
-    val APPLICATION_VND_OASIS_OPENDOCUMENT_TEXT = MediaType.of("application/vnd.oasis.opendocument.text", Charsets.UTF_8)
+    val APPLICATION_VND_OASIS_OPENDOCUMENT_TEXT = mediaType("application/vnd.oasis.opendocument.text")
     @JvmField
     val APPLICATION_VND_OPENXMLFORMATS_OFFICEDOCUMENT_PRESENTATIONML_PRESENTATION =
-        MediaType.of("application/vnd.openxmlformats-officedocument.presentationml.presentation", Charsets.UTF_8)
+        mediaType("application/vnd.openxmlformats-officedocument.presentationml.presentation")
     @JvmField
     val APPLICATION_VND_OPENXMLFORMATS_OFFICEDOCUMENT_PRESENTATIONML_TEMPLATE =
-        MediaType.of("application/vnd.openxmlformats-officedocument.presentationml.template", Charsets.UTF_8)
+        mediaType("application/vnd.openxmlformats-officedocument.presentationml.template")
     @JvmField
     val APPLICATION_VND_OPENXMLFORMATS_OFFICEDOCUMENT_SPREADSHEETML_SHEET =
-        MediaType.of("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", Charsets.UTF_8)
+        mediaType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
     @JvmField
     val APPLICATION_VND_OPENXMLFORMATS_OFFICEDOCUMENT_SPREADSHEETML_TEMPLATE =
-        MediaType.of("application/vnd.openxmlformats-officedocument.spreadsheetml.template", Charsets.UTF_8)
+        mediaType("application/vnd.openxmlformats-officedocument.spreadsheetml.template")
     @JvmField
     val APPLICATION_VND_OPENXMLFORMATS_OFFICEDOCUMENT_WORDPROCESSINGML_DOCUMENT =
-        MediaType.of("application/vnd.openxmlformats-officedocument.wordprocessingml.document", Charsets.UTF_8)
+        mediaType("application/vnd.openxmlformats-officedocument.wordprocessingml.document")
     @JvmField
     val APPLICATION_VND_OPENXMLFORMATS_OFFICEDOCUMENT_WORDPROCESSINGML_TEMPLATE =
-        MediaType.of("application/vnd.openxmlformats-officedocument.wordprocessingml.template", Charsets.UTF_8)
+        mediaType("application/vnd.openxmlformats-officedocument.wordprocessingml.template")
     @JvmField
-    val APPLICATION_VND_VISIO = MediaType.of("application/vnd.visio", Charsets.UTF_8)
+    val APPLICATION_VND_VISIO = mediaType("application/vnd.visio")
     @JvmField
-    val APPLICATION_XHTML_XML = MediaType.of("application/xhtml+xml", Charsets.UTF_8)
+    val APPLICATION_XHTML_XML = mediaType("application/xhtml+xml")
     @JvmField
-    val APPLICATION_XML = MediaType.of("application/xml", Charsets.UTF_8)
+    val APPLICATION_XML = mediaType("application/xml")
     @JvmField
-    val APPLICATION_X_7Z_COMPRESSED = MediaType.of("application/x-7z-compressed", Charsets.UTF_8)
+    val APPLICATION_X_7Z_COMPRESSED = mediaType("application/x-7z-compressed")
     @JvmField
-    val APPLICATION_X_ABIWORD = MediaType.of("application/x-abiword", Charsets.UTF_8)
+    val APPLICATION_X_ABIWORD = mediaType("application/x-abiword")
     @JvmField
-    val APPLICATION_X_BZIP = MediaType.of("application/x-bzip", Charsets.UTF_8)
+    val APPLICATION_X_BZIP = mediaType("application/x-bzip")
     @JvmField
-    val APPLICATION_X_BZIP2 = MediaType.of("application/x-bzip2", Charsets.UTF_8)
+    val APPLICATION_X_BZIP2 = mediaType("application/x-bzip2")
     @JvmField
-    val APPLICATION_X_CSH = MediaType.of("application/x-csh", Charsets.UTF_8)
+    val APPLICATION_X_CSH = mediaType("application/x-csh")
     @JvmField
-    val APPLICATION_X_RAR_COMPRESSED = MediaType.of("application/x-rar-compressed", Charsets.UTF_8)
+    val APPLICATION_X_RAR_COMPRESSED = mediaType("application/x-rar-compressed")
     @JvmField
-    val APPLICATION_X_SH = MediaType.of("application/x-sh", Charsets.UTF_8)
+    val APPLICATION_X_SH = mediaType("application/x-sh")
     @JvmField
-    val APPLICATION_X_SHOCKWAVE_FLASH = MediaType.of("application/x-shockwave-flash", Charsets.UTF_8)
+    val APPLICATION_X_SHOCKWAVE_FLASH = mediaType("application/x-shockwave-flash")
     @JvmField
-    val APPLICATION_X_TAR = MediaType.of("application/x-tar", Charsets.UTF_8)
+    val APPLICATION_X_TAR = mediaType("application/x-tar")
     @JvmField
-    val APPLICATION_ZIP = MediaType.of("application/zip", Charsets.UTF_8)
+    val APPLICATION_ZIP = mediaType("application/zip")
     @JvmField
-    val AUDIO_AAC = MediaType.of("audio/aac", Charsets.UTF_8)
+    val AUDIO_AAC = mediaType("audio/aac")
     @JvmField
-    val AUDIO_MIDI = MediaType.of("audio/midi", Charsets.UTF_8)
+    val AUDIO_MIDI = mediaType("audio/midi")
     @JvmField
-    val AUDIO_OGG = MediaType.of("audio/ogg", Charsets.UTF_8)
+    val AUDIO_OGG = mediaType("audio/ogg")
     @JvmField
-    val AUDIO_WAV = MediaType.of("audio/wav", Charsets.UTF_8)
+    val AUDIO_WAV = mediaType("audio/wav")
     @JvmField
-    val AUDIO_WEBM = MediaType.of("audio/webm", Charsets.UTF_8)
+    val AUDIO_WEBM = mediaType("audio/webm")
     @JvmField
-    val FONT_OTF = MediaType.of("font/otf", Charsets.UTF_8)
+    val FONT_OTF = mediaType("font/otf")
     @JvmField
-    val FONT_TTF = MediaType.of("font/ttf", Charsets.UTF_8)
+    val FONT_TTF = mediaType("font/ttf")
     @JvmField
-    val FONT_WOFF = MediaType.of("font/woff", Charsets.UTF_8)
+    val FONT_WOFF = mediaType("font/woff")
     @JvmField
-    val FONT_WOFF2 = MediaType.of("font/woff2", Charsets.UTF_8)
+    val FONT_WOFF2 = mediaType("font/woff2")
     @JvmField
-    val IMAGE_GIF = MediaType.of("image/gif", Charsets.UTF_8)
+    val IMAGE_GIF = mediaType("image/gif")
     @JvmField
-    val IMAGE_JPEG = MediaType.of("image/jpeg", Charsets.UTF_8)
+    val IMAGE_JPEG = mediaType("image/jpeg")
     @JvmField
-    val IMAGE_PNG = MediaType.of("image/png", Charsets.UTF_8)
+    val IMAGE_PNG = mediaType("image/png")
     @JvmField
-    val IMAGE_SVG_XML = MediaType.of("image/svg+xml", Charsets.UTF_8)
+    val IMAGE_SVG_XML = mediaType("image/svg+xml")
     @JvmField
-    val IMAGE_TIFF = MediaType.of("image/tiff", Charsets.UTF_8)
+    val IMAGE_TIFF = mediaType("image/tiff")
     @JvmField
-    val IMAGE_WEBP = MediaType.of("image/webp", Charsets.UTF_8)
+    val IMAGE_WEBP = mediaType("image/webp")
     @JvmField
-    val IMAGE_X_ICON = MediaType.of("image/x-icon", Charsets.UTF_8)
+    val IMAGE_X_ICON = mediaType("image/x-icon")
     @JvmField
-    val TEXT_CALENDAR = MediaType.of("text/calendar", Charsets.UTF_8)
+    val TEXT_CALENDAR = mediaType("text/calendar")
     @JvmField
-    val TEXT_CSS = MediaType.of("text/css", Charsets.UTF_8)
+    val TEXT_CSS = mediaType("text/css")
     @JvmField
-    val TEXT_CSV = MediaType.of("text/csv", Charsets.UTF_8)
+    val TEXT_CSV = mediaType("text/csv")
     @JvmField
-    val TEXT_HTML = MediaType.of("text/html", Charsets.UTF_8)
+    val TEXT_HTML = mediaType("text/html")
     @JvmField
-    val TEXT_PLAIN = MediaType.of("text/plain", Charsets.UTF_8)
+    val TEXT_PLAIN = mediaType("text/plain")
     @JvmField
-    val TEXT_XML = MediaType.of("text/xml", Charsets.UTF_8)
+    val TEXT_XML = mediaType("text/xml")
     @JvmField
-    val VIDEO_3GPP = MediaType.of("video/3gpp", Charsets.UTF_8)
+    val VIDEO_3GPP = mediaType("video/3gpp")
     @JvmField
-    val VIDEO_3GPP2 = MediaType.of("video/3gpp2", Charsets.UTF_8)
+    val VIDEO_3GPP2 = mediaType("video/3gpp2")
     @JvmField
-    val VIDEO_MPEG = MediaType.of("video/mpeg", Charsets.UTF_8)
+    val VIDEO_MPEG = mediaType("video/mpeg")
     @JvmField
-    val VIDEO_OGG = MediaType.of("video/ogg", Charsets.UTF_8)
+    val VIDEO_OGG = mediaType("video/ogg")
     @JvmField
-    val VIDEO_WEBM = MediaType.of("video/webm", Charsets.UTF_8)
+    val VIDEO_WEBM = mediaType("video/webm")
     @JvmField
-    val VIDEO_X_MSVIDEO = MediaType.of("video/x-msvideo", Charsets.UTF_8)
+    val VIDEO_X_MSVIDEO = mediaType("video/x-msvideo")
 }
