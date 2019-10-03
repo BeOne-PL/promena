f402f9c46133f931df19ec90c59548dcc1c399b3
Bartosz Skotarek <bzskotarek@gmail.com>
2019-10-03 15:00:18 +0200

    [added][promena-connector-activemq] ComponentScan to configuration

--- module/connector/activemq/configuration/src/main/kotlin/pl/beone/promena/connector/activemq/configuration/ActiveMQConnectorModuleConfig.kt
+++ module/connector/activemq/configuration/src/main/kotlin/pl/beone/promena/connector/activemq/configuration/ActiveMQConnectorModuleConfig.kt
@@ -1,8 +1,10 @@
 package pl.beone.promena.connector.activemq.configuration
 
+import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.PropertySource
 
 @Configuration
+@ComponentScan("pl.beone.promena.connector.activemq.configuration")
 @PropertySource("classpath:module-connector-activemq.properties")
 class ActiveMQConnectorModuleConfig
