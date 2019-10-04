cfb6dec94e7bf4aa4331f2faad708ee338823251
Bartosz Skotarek <bzskotarek@gmail.com>
2019-10-04 19:12:23 +0200

    [fixed][promena-core] Parsing days and hours in toDuration function

--- base/promena-core/configuration/src/main/kotlin/pl/beone/promena/core/configuration/external/akka/transformation/AkkaTransformerServiceContext.kt
+++ base/promena-core/configuration/src/main/kotlin/pl/beone/promena/core/configuration/external/akka/transformation/AkkaTransformerServiceContext.kt
@@ -29,8 +29,8 @@ class AkkaTransformerServiceContext {
 
     private fun String.toDuration(): Duration {
         val formatter = PeriodFormatterBuilder()
-            .appendDays().appendSuffix("d ")
-            .appendHours().appendSuffix("h ")
+            .appendDays().appendSuffix("d")
+            .appendHours().appendSuffix("h")
             .appendMinutes().appendSuffix("m")
             .appendSeconds().appendSuffix("s")
             .appendMillis().appendSuffix("ms")
