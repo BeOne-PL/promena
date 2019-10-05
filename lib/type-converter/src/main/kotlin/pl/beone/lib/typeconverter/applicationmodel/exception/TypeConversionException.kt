a8ba4d2abe61409d7f40dbc044ef666ea9007d29
Bartosz Skotarek <bzskotarek@gmail.com>
2019-10-05 16:43:29 +0200

    [improved][type-converter] Extracted to a separate repository

--- lib/type-converter/src/main/kotlin/pl/beone/lib/typeconverter/applicationmodel/exception/TypeConversionException.kt
+++
@@ -1,6 +0,0 @@
-package pl.beone.lib.typeconverter.applicationmodel.exception
-
-class TypeConversionException(
-    message: String?,
-    cause: Throwable? = null
-) : RuntimeException(message, cause)
