828d1cfaa761cdd20424d31751a58d79e2dc528e
Bartosz Skotarek <bzskotarek@gmail.com>
2019-10-06 19:37:14 +0200

    [refactored][promena-core] Removed unused function

--- base/promena-core/integration-test/src/test/kotlin/pl/beone/promena/core/external/akka/transformation/AkkaTransformationServiceTestIT.kt
+++ base/promena-core/integration-test/src/test/kotlin/pl/beone/promena/core/external/akka/transformation/AkkaTransformationServiceTestIT.kt
@@ -343,11 +343,4 @@ class AkkaTransformationServiceTestIT {
 
     private fun Data.getString(): String =
         String(getBytes())
-
-    private fun Exception.getStringStackTrace(): String {
-        val sw = StringWriter()
-        val pw = PrintWriter(sw)
-        this.printStackTrace(pw)
-        return sw.toString()
-    }
 }
