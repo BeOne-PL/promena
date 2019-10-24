ad4227819402c04027b63ce48398a9a8b74bf12c
Bartosz Skotarek <bzskotarek@gmail.com>
2019-10-24 17:53:13 +0200

    [removed][alfresco-promena-core] Throws from signature

--- alfresco/alfresco-promena-core/src/main/kotlin/pl/beone/promena/alfresco/module/core/contract/node/NodesExistenceVerifier.kt
+++ alfresco/alfresco-promena-core/src/main/kotlin/pl/beone/promena/alfresco/module/core/contract/node/NodesExistenceVerifier.kt
@@ -1,10 +1,8 @@
 package pl.beone.promena.alfresco.module.core.contract.node
 
-import org.alfresco.service.cmr.repository.InvalidNodeRefException
 import org.alfresco.service.cmr.repository.NodeRef
 
 interface NodesExistenceVerifier {
 
-    @Throws(InvalidNodeRefException::class)
     fun verify(nodeRefs: List<NodeRef>)
 }
