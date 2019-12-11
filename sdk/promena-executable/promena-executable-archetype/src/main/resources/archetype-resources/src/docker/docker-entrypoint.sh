807fbb5d118901b5e957699b61ed847dfcdbcc71
Bartosz Skotarek <bzskotarek@gmail.com>
2019-12-11 19:59:17 +0100

    [improved][promena-executable-archetype] Default memory settings

--- sdk/promena-executable/promena-executable-archetype/src/main/resources/archetype-resources/src/docker/docker-entrypoint.sh
+++ sdk/promena-executable/promena-executable-archetype/src/main/resources/archetype-resources/src/docker/docker-entrypoint.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 set -e
 
-: ${JAVA_OPTS_MEMORY:='-Xms1536m -Xmx1536m -XX:NewSize=512m -XX:MaxNewSize=512m'}
-: ${JAVA_OPTS_GC:=''}
-: ${JAVA_OPTS_DEBUG:='-agentlib:jdwp=transport=dt_socket,address=*:9999,suspend=n,server=y'}
-: ${JAVA_OPTS_ADDITIONAL:='-Dfile.encoding=UTF-8 -Djava.security.egd=file:/dev/./urandom'}
-
 if [[ $@ ]]; then
-    exec "$@"
+  exec "$@"
 else
-    CMD="java $JAVA_OPTS_MEMORY $JAVA_OPTS_GC $JAVA_OPTS_ADDITIONAL $JAVA_OPTS_DEBUG -jar /opt/$APP_JAR"
+  : ${JAVA_OPTS_MEMORY:='-XX:MinRAMPercentage=50 -XX:MaxRAMPercentage=80'}
+  : ${JAVA_OPTS_GC:=''}
+  : ${JAVA_OPTS_DEBUG_ENABLED:='false'}
+  : ${JAVA_OPTS_DEBUG:='-agentlib:jdwp=transport=dt_socket,address=*:9999,suspend=n,server=y'}
+  : ${JAVA_OPTS_ADDITIONAL:='-Dfile.encoding=UTF-8 -Djava.security.egd=file:/dev/./urandom'}
+  : ${JAVA_OPTS_CUSTOM:=''}
 
-    echo "# Running <$CMD>..."
-    exec bash -c "$CMD"
+  if [ $JAVA_OPTS_DEBUG_ENABLED = "true" ]; then DEBUG=$JAVA_OPTS_DEBUG; else DEBUG=""; fi
+  CMD="java $JAVA_OPTS_MEMORY $JAVA_OPTS_GC $JAVA_OPTS_ADDITIONAL $JAVA_OPTS_CUSTOM $DEBUG -jar /opt/$APP_JAR"
+
+  echo "# Running <$CMD>..."
+  exec bash -c "$CMD"
 fi
