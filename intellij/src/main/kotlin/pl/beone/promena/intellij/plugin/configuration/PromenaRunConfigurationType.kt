36382fbdee866910b7afea05b4769b2bdcb44972
Bartosz Skotarek <bzskotarek@gmail.com>
2020-01-12 18:47:57 +0100

    [added][intellij] Icons

--- intellij/src/main/kotlin/pl/beone/promena/intellij/plugin/configuration/PromenaRunConfigurationType.kt
+++ intellij/src/main/kotlin/pl/beone/promena/intellij/plugin/configuration/PromenaRunConfigurationType.kt
@@ -2,9 +2,12 @@ package pl.beone.promena.intellij.plugin.configuration
 
 import com.intellij.execution.configurations.ConfigurationFactory
 import com.intellij.execution.configurations.ConfigurationType
+import com.intellij.openapi.util.IconLoader
 import javax.swing.Icon
 
 object PromenaRunConfigurationType : ConfigurationType {
+
+    private val icon = IconLoader.getIcon("/icon/promena.svg", PromenaRunConfigurationType::class.java)
 
     override fun getDisplayName(): String =
         "Promena"
@@ -12,8 +15,8 @@ object PromenaRunConfigurationType : ConfigurationType {
     override fun getConfigurationTypeDescription(): String? =
         null
 
-    override fun getIcon(): Icon? =
-        null
+    override fun getIcon(): Icon =
+        icon
 
     override fun getId(): String =
         "PROMENA_RUN_CONFIGURATION"
