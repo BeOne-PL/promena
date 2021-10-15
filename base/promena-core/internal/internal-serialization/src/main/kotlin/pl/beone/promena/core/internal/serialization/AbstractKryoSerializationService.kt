0f8a144dae638f06e4b5ac2f5fb6e4ba47816ef0
kamil_jedrzejczak <kamil.jedrzejczak@beone.pl>
2021-10-15 12:10:18 +0100

    Reduce serialization time

--- base/promena-core/internal/internal-serialization/src/main/kotlin/pl/beone/promena/core/internal/serialization/AbstractKryoSerializationService.kt
+++ base/promena-core/internal/internal-serialization/src/main/kotlin/pl/beone/promena/core/internal/serialization/AbstractKryoSerializationService.kt
@@ -3,9 +3,11 @@ package pl.beone.promena.core.internal.serialization
 import com.esotericsoftware.kryo.Kryo
 import com.esotericsoftware.kryo.io.ByteBufferInput
 import com.esotericsoftware.kryo.io.ByteBufferOutput
+import com.esotericsoftware.kryo.io.Output
 import pl.beone.promena.core.applicationmodel.exception.serializer.DeserializationException
 import pl.beone.promena.core.applicationmodel.exception.serializer.SerializationException
 import pl.beone.promena.core.contract.serialization.SerializationService
+import java.io.ByteArrayOutputStream
 
 abstract class AbstractKryoSerializationService(
     private val bufferSize: Int
@@ -13,9 +15,15 @@ abstract class AbstractKryoSerializationService(
 
     override fun <T> serialize(element: T): ByteArray =
         try {
-            with(ByteBufferOutput(bufferSize)) {
+            val os = ByteArrayOutputStream()
+            with(Output(os)) {
                 getKryo().writeClassAndObject(this, element)
-                this.toBytes()
+                os.writeBytes(this.toBytes())
+
+                if (os.size() > bufferSize) {
+                    throw SerializationException("Serialization failed. Insufficient buffer size.")
+                }
+                os.toByteArray()
             }
         } catch (e: Exception) {
             throw SerializationException("Couldn't serialize", e)
